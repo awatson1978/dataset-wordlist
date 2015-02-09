8d4806db5e17dfbe1dd48e028103a6ba149e8c03
Abigail Watson <awatson1978@gmail.com>
2015-02-08 23:00:29 -0500

    need to export WordList

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
   summary: "Meteorite package that adds the Fry Wordlist of 1000 most commonly used words.",
 
-    // update this value before you run 'meteor publish'
-    version: "1.0.1",
+  // update this value before you run 'meteor publish'
+  version: "1.0.2",
 
-    // if this value isn't set, meteor will default to the directory name
-    name: "awatson1978:dataset-wordlist",
+  // if this value isn't set, meteor will default to the directory name
+  name: "awatson1978:dataset-wordlist",
 
-    // and add this value if you want people to access your code from Atmosphere
-    git: "http://github.com/awatson1978/dataset-wordlist.git"
+  // and add this value if you want people to access your code from Atmosphere
+  git: "http://github.com/awatson1978/dataset-wordlist.git"
 });
 
 Package.on_use(function (api) {
-    api.use('standard-app-packages@1.0.4');
-    api.add_files('initialize.wordlist.js', ["client","server"]);
+  api.export('WordList');
+  api.use('standard-app-packages@1.0.4');
+  api.add_files('initialize.wordlist.js', ["client","server"]);
 });
