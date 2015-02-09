45f1efa9406de025889b04556c6f586f38ed02d4
Abigail Watson <awatson1978@gmail.com>
2015-02-08 20:03:28 -0500

    isopack upgrade

--- package.js
+++ package.js
@@ -1,8 +1,17 @@
 Package.describe({
-  summary: "Meteorite package that adds the Fry Wordlist of 1000 most commonly used words."
+  summary: "Meteorite package that adds the Fry Wordlist of 1000 most commonly used words.",
+
+    // update this value before you run 'meteor publish'
+    version: "1.0.0",
+
+    // if this value isn't set, meteor will default to the directory name
+    name: "awatson1978:dataset-wordlist",
+
+    // and add this value if you want people to access your code from Atmosphere
+    git: "http://github.com/awatson1978/dataset-wordlist.git"
 });
 
 Package.on_use(function (api) {
-    api.use('standard-app-packages');
+    api.use('standard-app-packages@1.0.4');
     api.add_files('initialize.wordlist.js', ["client","server"]);
 });
